ff1d2fccf71ae3cc4e1a9bffaf6ca13d9f242472
Thomas Ruschival <thomas@ruschival.de>
2025-11-28 15:38:06 +0100

    Some more options + Doc
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- birdrecorder/cli.py
+++ birdrecorder/cli.py
@@ -20,31 +20,50 @@ def open_stream(args) -> cv2.VideoCapture:
     if not ret:
         raise SystemError(f"Error reading from source {ret}")
 
+
+    logger.info(f"Autofocus {cap.get(cv2.CAP_PROP_AUTOFOCUS)}")
+    logger.info(f"Auto exposure {cap.get(cv2.CAP_PROP_AUTO_EXPOSURE)}")
+    logger.info(f"Auto white balance {cap.get(cv2.CAP_PROP_AUTO_WB)}")
+    logger.info(f"White balance temperature {cap.get(cv2.CAP_PROP_WB_TEMPERATURE)}")
+    logger.info(f"Brightness {cap.get(cv2.CAP_PROP_BRIGHTNESS)}")
+    logger.info(f"Contrast {cap.get(cv2.CAP_PROP_CONTRAST)}")
+
+    if cap.set(cv2.CAP_PROP_AUTOFOCUS, int(args.auto_focus)):
+        logger.info(f"Setting auto focus: {args.auto_focus}")
     if cap.set(cv2.CAP_PROP_AUTO_WB, int(args.auto_white_balance)):
-        logger.info(f"Auto white balance: {args.auto_white_balance}")
+        logger.info(f"Setting auto white balance: {args.auto_white_balance}")
     if cap.set(cv2.CAP_PROP_WB_TEMPERATURE, int(args.color_temp)):
-        logger.info(f"Set color temperature to {args.color_temp}")
+        logger.info(f"Setting color temperature to {args.color_temp}")
 
     return cap
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Motion detection and recording")
+
     parser.add_argument(
         "--detection",
         choices=["yolo", "opencv"],
         default="yolo",
         help="Detection method: yolo for object detection, opencv for motion detection (default: yolo)",
     )
+
     parser.add_argument(
         "--min-area",
         type=int,
-        default=500,
-        help="Minimum area for motion detection (default: 500)",
+        default=1000,
+        help="Minimum area for motion detection (default: 1000)",
     )
 
     parser.add_argument(
         "--source", type=str, default=0, help="Camera device or video file path"
+    )
+
+    parser.add_argument(
+        "--hysteresis",
+        type=int,
+        default=15,
+        help="Number of frames with(out) motion or object until recording starts/stops (default: 15)",
     )
 
     parser.add_argument(
@@ -56,11 +75,11 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--mark",
+        "--auto-focus",
         type=bool,
         default=False,
         action=argparse.BooleanOptionalAction,
-        help="Mark detected objects on frame",
+        help="camera auto focus",
     )
 
     parser.add_argument(
@@ -69,6 +88,15 @@ def parse_args():
         default=5500,
         help="Color correction temperature (if --disable-auto-white-balance)",
     )
+
+    parser.add_argument(
+        "--mark",
+        type=bool,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="Mark detected objects on frame",
+    )
+
 
     return parser.parse_args()
 
@@ -111,20 +139,20 @@ def main():
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     fps = cap.get(cv2.CAP_PROP_FPS)
+
     logger.info(f"Camera opened: {width}x{height} @ {fps}fps")
-    recorder = Recorder(Path("."), width, height, fps, CircularFrameStore(150))
-    recording_hysteresis = Hysteresis(recorder, start_delay=15, stop_delay=30)
+    recorder = Recorder(Path("."), width, height, fps, CircularFrameStore(3*args.hysteresis))
+    recording_hysteresis = Hysteresis(recorder, start_delay=args.hysteresis, stop_delay=args.hysteresis)
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        timestamp_frame(frame)
         masked_frame = mask_frame(frame)
 
         boxes = detector.detect(masked_frame)
-        # Did we find somthing -> add to hysteresis
+        # Did we find something -> add to hysteresis
         recording_hysteresis.step(len(boxes) > 0)
 
         # If recording, annotate the frame
@@ -133,6 +161,7 @@ def main():
                 box.draw_on_frame(frame)
 
         # The recorder will discard if inactive
+        timestamp_frame(frame)
         recorder.append_frame(frame)
 
         # Show for debugging
